a6098a59f76242e875d9b9e946a3bdf164003f5a
Ian Carpenter <ianhcarpenter@gmail.com>
2018-07-12 21:02:45 +0100

    Initial commit

--- AirportExplorer2/Pages/Index.cshtml.cs
+++ AirportExplorer2/Pages/Index.cshtml.cs
@@ -1,17 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using GoogleApi;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Places.Details.Request;
+using GoogleApi.Entities.Places.Photos.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AirportExplorer2.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public string MapboxAccessToken { get; }
+        public string GoogleApiKey { get; }
+
+        public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+
+            MapboxAccessToken = configuration["Mapbox:AccessToken"];
+            GoogleApiKey = configuration["google:ApiKey"];
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnGetAirports()
+        {
+            var configuration = new Configuration
+            {
+                BadDataFound = context => { Console.WriteLine($"Bad data: <{context.RawRecord}>"); }
+            };           
+
+            using (var sr = new StreamReader(Path.Combine(_hostingEnvironment.WebRootPath, "airports.dat")))
+            using (var reader = new CsvReader(sr, configuration))
+            {
+                FeatureCollection featureCollection = new FeatureCollection();
+
+                while (reader.Read())
+                {
+                    try
+                    {
+                        string name = reader.GetField<string>(1);
+                        string iataCode = reader.GetField<string>(4);
+                        double latitude = reader.GetField<double>(6);
+                        double longitude = reader.GetField<double>(7);
+
+                        featureCollection.Features.Add(new Feature(
+                            new Point(new Position(latitude, longitude)),
+                            new Dictionary<string, object>
+                            {
+                            {"name", name },
+                            {"iataCode", iataCode }
+                            }));
+                    } 
+                    catch (BadDataException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    
+                }
+                return new JsonResult(featureCollection);
+            }                                
+        }
+
+        public async Task<IActionResult> OnGetAirportDetail(string name, double latitude, double longitude)
+        {
+            var airportDetail = new AirportDetail();
+
+            // Execute the search request
+            var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
+            {
+                Key = GoogleApiKey,
+                Name = name,
+                Location = new Location(latitude, longitude),
+                Radius = 1000
+            });
+
+            // If we did not get a good response, or the list of results are empty then get out of here
+            if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
+                return new BadRequestResult();
+
+            // Get the first result
+            var nearbyResult = searchResponse.Results.FirstOrDefault();
+            string placeId = nearbyResult.PlaceId;
+            string photoReference = nearbyResult.Photos?.FirstOrDefault()?.PhotoReference;
+            string photoCredit = nearbyResult.Photos?.FirstOrDefault()?.HtmlAttributions.FirstOrDefault();
+
+            // Execute the details request
+            var detailsResponse = await GooglePlaces.Details.QueryAsync(new PlacesDetailsRequest
+            {
+                Key = GoogleApiKey,
+                PlaceId = placeId
+            });
+
+            // If we did not get a good response then get out of here
+            if (!detailsResponse.Status.HasValue || detailsResponse.Status.Value != Status.Ok)
+                return new BadRequestResult();
+
+            // Set the details
+            var detailsResult = detailsResponse.Result;
+            airportDetail.FormattedAddress = detailsResult.FormattedAddress;
+            airportDetail.PhoneNumber = detailsResult.InternationalPhoneNumber;
+            airportDetail.Website = detailsResult.Website;
+
+            if (photoReference != null)
+            {
+                // Execute the photo request
+                var photosResponse = await GooglePlaces.Photos.QueryAsync(new PlacesPhotosRequest
+                {
+                    Key = GoogleApiKey,
+                    PhotoReference = photoReference,
+                    MaxWidth = 400
+                });
+                
+                if (photosResponse.PhotoBuffer != null)
+                {
+                    airportDetail.Photo = Convert.ToBase64String(photosResponse.PhotoBuffer);
+                    airportDetail.PhotoCredit = photoCredit;
+
+                }                
+            } 
+            return new JsonResult(airportDetail);
 
         }
+
     }
 }
